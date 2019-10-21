55cded00abd6e2fa0f249a7e3636ff21db3fa045
Michael Weedman <michael.weedman@gmail.com>
2019-10-21 11:58:59 -0700

    Added Year of Birth Function

--- drills1.js
+++ drills1.js
@@ -1,4 +1,8 @@
+function getYearOfBirth(age){
+    return 2019 - age;
+}
+
 function createGreeting(name, age){
-  const yearOf = 2019 - age;
+    const yearOf = 2019 - age;
     return `Hi my name is ${name} and I am ${age} years old. I was born on ${yearOf}`
 }
