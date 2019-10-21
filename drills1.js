b89a3c985526cc5d9bd75b340367b56d43057dcf
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-21 12:23:25 -0700

    threw an error and a try and catch conditional

--- drills1.js
+++ drills1.js
@@ -1,8 +1,22 @@
 function getYearOfBirth(age){
+    if (age < 0){
+        throw new Error ("Age can not be negative!!");
+    }
+    
     return 2019 - age;
+
 }
 
 function createGreeting(name, age){
     const yearOf = 2019 - age;
-    return `Hi my name is ${name} and I am ${age} years old. I was born on ${yearOf}`
+    return `Hi my name is ${name} and I am ${age} years old. I was born on ${yearOf}`;
+}
+try {
+
+    const newGreeting = createGreeting(name, age);
+    return newGreeting;
+    
+
+}catch (err){
+    console.error('Age cannot be less than 0');
 }
