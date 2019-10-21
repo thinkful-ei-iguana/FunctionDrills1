2547a3b398a141e2de961c5c2d241f687626e7f5
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-21 11:56:57 -0700

    I added a variable called yearOf

--- drills1.js
+++ drills1.js
@@ -1,3 +1,4 @@
 function createGreeting(name, age){
-  return `Hi my name is ${name} and I am ${age} years old.`
+  const yearOf = 2019 - age;
+    return `Hi my name is ${name} and I am ${age} years old. I was born on ${yearOf}`
 }
